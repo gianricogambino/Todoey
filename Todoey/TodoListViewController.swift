821e9ac4482781b8dfa11c9c9300066de7917ad2
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-19 10:55:58 +0100

    TableViewController added delegate methods to select and deselect a row in the list

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -17,7 +17,8 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    //MARK - TableView Datasource Methods
+    //MARK: - TableView Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -28,5 +29,19 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //attiva l'effetto a scomparsa della selezione della riga
+        tableView.deselectRow(at: indexPath, animated: true)
+        //attiva la selezione e deselezione della riga col checkmark
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+
 }
 
