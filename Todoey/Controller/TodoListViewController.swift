097eb4e5eea89b52b6b5f26fd935af1576a3f805
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-21 22:42:58 +0100

    Deleted al the comments

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -14,32 +14,8 @@ class TodoListViewController: UITableViewController {
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    //userdefaults non va bene perché carica tutte le info in blocco e a volte non serve
-//    let defaults = UserDefaults.standard
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //per caricare i dati salvati in locale con if let per non farlo crashare se per caso non ci fossero i dati
-        
-//        let newItem = Item()
-//        newItem.title = "Primo elemento"
-//        itemArray.append(newItem)
-//
-//        let newItem2 = Item()
-//        newItem2.title = "Secondo elemento"
-//        itemArray.append(newItem2)
-//
-//        let newItem3 = Item()
-//        newItem3.title = "Terzo elemento"
-//        itemArray.append(newItem3)
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itemArray = items
-//        }
-        // al posto di def<ults chiamiamo un nuovo metodo che ricarica i nostri dati dal plist
         
         loadItems()
         
@@ -54,17 +30,8 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        //con la let item non ripetiamo venti volte itemArray[indexPath.row]
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
-        //qui sostituiamo questo if con un operatore ternario
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
-        // OPERATORE TERNARIO
-        // valore = condizione ? valoreSeVero : valoreSeFalso
         cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
@@ -72,13 +39,6 @@ class TodoListViewController: UITableViewController {
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        TUTTE QUESTE RIGHE
-//        if itemArray[indexPath.row].done == false {
-//            itemArray[indexPath.row].done = true
-//        } else {
-//            itemArray[indexPath.row].done = false
-//        }
-//        SONO SOSTITUITA DA UNA SOLA RIGA QUI SOTTO
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
@@ -88,23 +48,13 @@ class TodoListViewController: UITableViewController {
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
-        //Costruiamo una variabile locale richiamabile anche da dentro la closure
         var textField = UITextField()
-        
-        //COSTRUISCEALERT CONTROLLER DENTRO CUI STANNO GLI ALERT ACTION
         let alert = UIAlertController(title: "Add new todoey item", message: "", preferredStyle: .alert)
-        
-        //PULSANTE E RELATIVA SUA AZIONE IN CLOSURE
         let action = UIAlertAction(title: "Add Item", style: .default, handler: { (action) in
-            
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
             self.saveItems()
-            //QUI CASCA LA CLOSURE ESEGUITA AL CLIC
-            //self.itemArray.append(textField.text!)
-            //self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            
         })
         
         alert.addTextField { (alertTextField) in
