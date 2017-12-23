5d242a81f6bda17a443b5abf14479e7bde923619
Gianrico Gambino <gianrico.gambino@gmail.com>
2017-12-23 18:08:30 +0100

    Realm:CRUD made D on TodoList

--- Todoey/Controller/TodoListViewController/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController/TodoListViewController.swift
@@ -55,6 +55,7 @@ class TodoListViewController: UITableViewController {
         if let item = todoItems?[indexPath.row] {
             do {
                 try realm.write {
+                    //realm.delete(item)
                     item.done = !item.done
                 }
             } catch {
